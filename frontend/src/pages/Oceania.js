00392cff48432e5665efd2b5297be4a784fb8c99
gseym <78279867+gseym@users.noreply.github.com>
2022-02-25 00:07:04 -0500

    Create Oceania.js

---
+++ frontend/src/pages/Oceania.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+function Oceania() {
+  return (<><div class="container2">
+  <div class="text-intro">
+  <center><h1>Oceania</h1></center> 
+    
+  </div>
+  <header class="page-header">
+    <nav>
+      <ul>
+        <li>
+          <a href="#Fiji">Fiji</a>
+        </li>
+        <li>
+          <a href="#Samoa ">Samoa</a>
+        </li>
+        <li>
+          <a href="#Vanuatu">Vanuatu</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="header-text">
+      Over 1,765,000 cases have been reported in Oceania. Many lockdowns in the countries of Oc
+eania has lead to a potential collapse of the tourism industry and international trade and travel has been greatly reduced. Tourism in this continent is often a large source of employment and its potential collapse has led to a lot of distress. 
+    </div>
+  </header>
+</div><section class="section" id="Fiji">
+    <div class="container2">
+     
+      <h2><font color="#FFFFFF">Fiji</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF">Fiji’s economy took a major hit, decreasing by about 19% in 2020, due to the COVID-19 pandemic. Additionally, the island continues to face climate-related emergencies which in addition to pandemic economic stresses has caused turmoil within the communities. Although the country was heavily impacted, it’s economy is working to bounce back to its previous pre-pandemic state. Places to visit include Ndai, Laucala Island, and Suva. </font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wMnd0MBooagwIBBIIL20vMDJ3dDASCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="Samoa">
+    <div class="container2">
+      <h2><font color="#FFFFFF">Samoa</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF">Despite being one of the first countries to shut down and thus be its borders, Samoa economy was greatly hit.
+Due to reduced tourism and limited job opportunities many Samoans are directly impacted. Places to visit in Samoa include To-Sua Ocean Trench, Lalomanu Beach, and Piula Cave pool. </font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wN3o1bhooagwIBBIIL20vMDd6NW4SCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgESGAEgASoMCAMSCC9tLzA2czl5&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="Vanuatu">
+    <div class="container2">
+      <h2><font color="#FFFFFF">Vanuatu</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF">Vanuatu revenue has taken a “bigger hit than GDP”. Sectors such as Agriculture fishing and forestry suffered not only from the pandemic but from environmental stressors. Places to visit include Mount Yasur, Champagne Beach, and Millennium Cave. </font>
+      </div>
+      <font color="#FFFFFF"><a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wN3o1bhooagwIBBIIL20vMDd6NW4SCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+       To flights →
+      </a></font>
+    </div>
+  </section><footer class="page-footer">
+   
+   
+  </footer></>);
+}
+
+export default Oceania;
