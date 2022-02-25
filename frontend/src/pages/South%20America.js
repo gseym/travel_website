26d26435ef735e330419feae33924e2ce87e1e24
gseym <78279867+gseym@users.noreply.github.com>
2022-02-25 00:07:44 -0500

    Create South%20America.js

---
+++ frontend/src/pages/South%20America.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+function SouthAmerica() {
+  return (<><div class="container2">
+  <div class="text-intro">
+  <center><h1>South America</h1></center> 
+    
+  </div>
+  <header class="page-header">
+    <nav>
+      <ul>
+        <li>
+          <a href="#Panama">Panama</a>
+        </li>
+        <li>
+          <a href="#Argentina">Argentina</a>
+        </li>
+        <li>
+          <a href="#Peru">Peru</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="header-text">
+    According to the World Bank the real GDP growth for Latin America has dropped to -4.6 percent,  22 million people in Latin America moved into poverty in 2020, with poverty rising to 33.7% from 30.5% in 2019
+
+    </div>
+  </header>
+</div><section class="section" id="Panama">
+    <div class="container2">
+     
+      <h2><font color="#FFFFFF">Panama</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF">  Panama, with a population of just over four million people, earned international headlines throughout the pandemic. It did so unwillingly because its health performance has been among the worst in the world at various times, and the pandemic's economic impact has been the most catastrophic. As a result, world public opinion appears surprised by what Panamanian individuals already know: the country is full of paradoxes, and there appears to be no middle ground.
+
+"Objectively," Panama was the Latin American country with the most recorded cases per 100,000 people at the beginning of July. Above Brazil, for example, which has been accused of deliberately infecting its citizens, which was not the case in Panama.
+</font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wNXF4MRooagwIBBIIL20vMDVxeDESCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="Argentina">
+    <div class="container2">
+      <h2><font color="#FFFFFF">Argentina</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF">The impact of COVID-19 on poverty in Argentina has been particularly severe because the country was already in a recession when the outbreak began. The unemployment rate in the country increased from 9.8% in 2019 to 11.7 percent in 2020. Young women, according to the International Labour Organization (ILO), will have the greatest unemployment rate. As a result, industries with a high number of female employees, such as tourism, hotel, and restaurant, would face the most difficult recovery from the epidemic. Even the roughly $24 billion spent by the Argentinian government on COVID-19, assistance programmes was unable to lift citizens totally out of financial losses due to an already unstable economy.</font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxonagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyCwgEEgcvbS8wamdkGidqCwgEEgcvbS8wamdkEgoyMDIyLTAyLTExcgwIAhIIL20vMDUycDdwAYIBCwj___________8BQAFIAZgBAbIBBBgBIAE&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="Peru">
+    <div class="container2">
+      <h2><font color="#FFFFFF">Peru</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF"> The COVID-19 outbreak has wreaked havoc across the country. A tight and protracted quarantine resulted in an 11.1 percent drop in GDP in 2020. Between April and December 2020, employment declined by an average of 20%. In response, the government launched a global economic compensation and aid programme to protect the vulnerable population and support enterprises, which includes cash transfers, tax delay, and private-sector credit guarantees. Despite this, the economic downturn and increased unemployment have pushed the poverty rate up by 6 percentage points to roughly 27% in 2020, putting nearly two million people in poverty.
+</font>
+      </div>
+      <font color="#FFFFFF"><a href="https://www.google.com/travel/explore?tfs=CBwQAxopagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDQgEEgkvbS8wMTZ3encaKWoNCAQSCS9tLzAxNnd6dxIKMjAyMi0wMi0xMXIMCAISCC9tLzA1MnA3cAGCAQsI____________AUABSAGYAQGyAQQYASAB&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+       To flights →
+      </a></font>
+    </div>
+  </section><footer class="page-footer">
+   
+   
+  </footer></>);
+}
+
+export default SouthAmerica;
