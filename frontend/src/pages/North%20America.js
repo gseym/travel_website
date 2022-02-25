5adeaf712519605bff1716359229b4a5d3271c10
gseym <78279867+gseym@users.noreply.github.com>
2022-02-25 00:06:38 -0500

    Create North%20America.js

---
+++ frontend/src/pages/North%20America.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+function NorthAmerica() {
+  return (<><div class="container2">
+  <div class="text-intro">
+  <center><h1>North America</h1></center> 
+    
+  </div>
+  <header class="page-header">
+    <nav>
+      <ul>
+        <li>
+          <a href="#Mexico">Mexico</a>
+        </li>
+        <li>
+          <a href="#CostaRica">Costa Rica</a>
+        </li>
+        <li>
+          <a href="#Jamaica ">Jamaica </a>
+        </li>
+      </ul>
+    </nav>
+    <div class="header-text">
+    The islands in the Caribbeans as well as Latin American countries depend greatly on American and Canadian tourism. With borders closing during the pandemic, many resorts and in leisure-specialized areas have been neglected and are in great need of clients. 
+    </div>
+  </header>
+</div><section class="section" id="Mexico">
+    <div class="container2">
+     
+      <h2><font color="#FFFFFF">Mexico</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF"> Tourism is Mexico's second-largest source of foreign money, after remittances and foreign direct investment, and is one of the country's most profitable economic sectors. However, measures to combat the Covid-19 outbreak, including flight cancellations and tight limits on people's mobility, have wreaked havoc on the tourism business, and with it, vast areas of the country. Panama's population has not been infected, which is not the case. </font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxopagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDQgEEgkvbS8wYjkwX3IaKWoNCAQSCS9tLzBiOTBfchIKMjAyMi0wMi0xMXIMCAISCC9tLzA1MnA3cAGCAQsI____________AUABSAGYAQGyAQQYASAB&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="CostaRica">
+    <div class="container2">
+      <h2><font color="#FFFFFF">Costa Rica </font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF">COVID-19 was the final straw for a country that had been on the verge of bankruptcy for decades. Costa Rica's finance minister recently warned that loan repayment will consume half of the country's budget in 2021, leaving the country with scant resources to fund pandemic assistance.
+
+President Carlos Alvarado Quesada was alerted, and he set out to address the fiscal imbalance before it worsened the impact of COVID-19 on poverty in Costa Rica. The administration quickly saw three options: promoting economic growth, raising taxes, or cutting spending. Costa Rica's administration followed the latter two options after nationwide lockdowns prohibited economic progress.
+</font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wMXA4cxooagwIBBIIL20vMDFwOHMSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="Jamaica">
+    <div class="container2">
+      <h2><font color="#FFFFFF">Jamaica</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF"> Since the pandemic began in 2020, COVID-19 has had a massive impact on poverty in Jamaica. Jamaica has always been a popular travel destination for those seeking sun, beaches, and culture. According to the World Bank, the country's annual tourism numbers increased to 4.2 million in 2019, more than doubling the previous two decades. However, after COVID-19 hit the world, Jamaica's tourism business has suffered as fewer people are able to visit the country.
+
+Businesses such as restaurants and resorts have seen a major drop in revenue. As a result, 50,000 Jamaicans employed in the tourism industry lost their employment, demonstrating COVID-19's significant impact on poverty in the country. As a result, many people who have successfully overcome poverty will almost certainly face this reality again.
+ </font>
+      </div>
+      <font color="#FFFFFF"><a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wM19yMxooagwIBBIIL20vMDNfcjMSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+       To flights →
+      </a></font>
+    </div>
+  </section><footer class="page-footer">
+   
+   
+  </footer></>);
+}
+
+export default NorthAmerica;
