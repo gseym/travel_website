0fa5e47dff371288ea0f2393002d8356a6d84d6a
gseym <78279867+gseym@users.noreply.github.com>
2022-02-25 00:03:08 -0500

    Create About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,31 @@
+import Infobox from '../components/Infobox.js';
+
+function About() {
+  return (
+    <>
+      <Infobox
+      text="Brune is in her U2 (junior) year at McGill University and is so glad to have been able to participate in this year's
+      McHacks with everyone! Aside from building websites, Brune spends her free time hiking, reading, and trying out new recipes." 
+      title='Meet our programmer, Brune Bettler!'
+      />
+      <Infobox
+      text="Gaby is in her U2 (junior) year at McGill University and this is her second year attending McHacks, after a successful 
+      2021 experience! Carrying over from her years growing up in Germany, Gaby also enjoys making dumplings and learning Russian." 
+      title='Meet our programmer, Gaby Le Bideau!'
+      />
+      <Infobox
+      text="Lucas is in his U2 (junior) year at McGill University and couldn't be more excited
+      to participate in this year's iteration of McHacks! When he's not busy hacking, Lucas enjoys
+      experimenting with classical piano composition, working out, and reading." 
+      title='Meet our programmer, Lucas Nelson!'
+      />
+      <Infobox
+      text="Gaby is in her U2 (junior) year at McGill University and is thrilled to be involved in this year's McHacks! After this hackathon wraps up,
+      Gaby plans to get back to discovering new music, and hopes to learn mixing soon." 
+      title='Meet our programmer, Gaby Seymour!'
+      />
+      
+    </>
+  )}
+
+export default About;
