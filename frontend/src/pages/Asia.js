bac49f0866cf101d538cd2fa3208ec33324a8b94
gseym <78279867+gseym@users.noreply.github.com>
2022-02-25 00:04:05 -0500

    Create Asia.js

---
+++ frontend/src/pages/Asia.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+function Asia() {
+  return(<><div class="container2">
+  <div class="text-intro">
+  <center><h1>Asia</h1></center> 
+    
+  </div>
+  <header class="page-header">
+    <nav>
+      <ul>
+        <li>
+          <a href="#Indonesia">Indonesia</a>
+        </li>
+        <li>
+          <a href="#Phillipines">Phillipines</a>
+        </li>
+        <li>
+          <a href="#India">India</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="header-text">
+    Pandemic-related lockdowns have “devastated” economies around the continent and have caused political changes and reevaluations. Of the total COVID-19 related deaths around the world, about 12%  were reported in the continent. Additionally, in developing Asia, millions of people were pushed into extreme poverty, leading to predicted delays in meeting global hunger and poverty reduction goals. 
+    </div>
+  </header>
+</div><section class="section" id="Indonesia">
+    <div class="container2">
+     
+      <h2><font color="#FFFFFF">Indonesia</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF"> Indonesia continues to be greatly impacted by the COVID-19 pandemic. Poverty rates largely increased since 2020 pushing over 1.3 million people into poverty. Although political turmoil bubbles, new leadership opportunities have become available for “middle-income” powers. Popular destinations include Bali, Jakarta, Ubud.</font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxopagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDQgEEgkvbS8wMWNyZDUaKWoNCAQSCS9tLzAxY3JkNRIKMjAyMi0wMi0xMXIMCAISCC9tLzA1MnA3cAGCAQsI____________AUABSAGYAQGyAQQYASAB&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="Phillipines">
+    <div class="container2">
+      <h2><font color="#FFFFFF">Phillipines</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF">In the Philippines, the COVID-19 pandemic hit rural communities the most by worsening preexisting economic and humanitarian challenges. Many communities were hit with a major loss of income and job opportunities. Construction, farming, and the public transportation sectors were witnesses to the greatest job losses in the country. Popular travel destinations in the Philippines includes Manila, Boracay, and Cebu City. 
+	</font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wNXY4YxooagwIBBIIL20vMDV2OGMSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="India">
+    <div class="container2">
+      <h2><font color="#FFFFFF">India</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF"> India’s economy was greatly impacted by the COVID-19 pandemic reaching even what has been announced as “India’s worst recession since independence”. The country’s GDP dropped by over 24% in 2020 and individual incomes dropped about 40% during the first lockdown. In order to help their economy and peoples, India plans to “prioritize economic expansion and sustainability” in order to  continue their growth and influence on the world stage. Must-visit places in India include Mumbai, Goa, and Jaipur. 
+
+</font>
+      </div>
+      <font color="#FFFFFF"><a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wZnY0dhooagwIBBIIL20vMGZ2NHYSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+       To flights →
+      </a></font>
+    </div>
+  </section><footer class="page-footer">
+   
+   
+  </footer></>);
+}
+
+export default Asia;
