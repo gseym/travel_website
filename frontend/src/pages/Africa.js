5d68e01dd75a6532965d63f1f1b8c46246db03be
gseym <78279867+gseym@users.noreply.github.com>
2022-02-25 00:03:38 -0500

    Create Africa.js

---
+++ frontend/src/pages/Africa.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+function Africa() {
+  return (<><div class="container2">
+    <div class="text-intro">
+    <center><h1>Africa</h1></center> 
+      
+    </div>
+    <header class="page-header">
+      <nav>
+        <ul>
+          <li>
+            <a href="#Mauritius">Mauritius</a>
+          </li>
+          <li>
+            <a href="#Morocco">Morocco</a>
+          </li>
+          <li>
+            <a href="#IvoryCoast">Ivory Coast</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="header-text">
+      Africa has been hit by the pandemic in a variety of ways due to the wide range of previous economic states of the different countries. Testing during the pandemic was difficult due to both lack of trained nurses and medical staff and health infrastructure. By the end of 2021, Africa accounted for about 4 percent of infections around the world. 
+      </div>
+    </header>
+  </div><section class="section" id="Mauritius">
+      <div class="container2">
+       
+        <h2><font color="#FFFFFF">Mauritius</font></h2>
+        <div class="section-text">
+        <font color="#FFFFFF"> In Mauritius, the economic impact of the COVID-19 pandemic has been detrimental. Lockdowns and restrictions have caused many business to shut down reducing jobs. Nature parks and beaches to explore include Belle Mare Plane, Chamarel Colored Earth, Trop Aux Cerfs. Popular tourist attractions include Port Louis, Grand Baie, and the Black River Gorges National Park.  </font>
+        </div>
+        <a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wNHZzORooagwIBBIIL20vMDR2czkSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+          To flights →
+        </a>
+      </div>
+    </section><section class="section" id="Morocco">
+      <div class="container2">
+        <h2><font color="#FFFFFF">Morocco</font></h2>
+        <div class="section-text">
+        <font color="#FFFFFF">In addition to harsh droughts and drops in major exports due to containment measures Morocco’s economy was greatly impacted by the pandemic. Following the government’s issue of a state of emergency lockdown, unemployment rates in the country greatly rose due to job losses across all sectors and regions. Of those most hit, workers in the agricultural sector were the most heavily impacted. Thankfully however, the country’s vaccination campaign has lead it to have the second-highest vaccination rate in Africa. Popular cities to visit include Marrakesh, Fes, and Casablanca. 
+</font>
+        </div>
+        <a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wNHdnaBooagwIBBIIL20vMDR3Z2gSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+          To flights →
+        </a>
+      </div>
+    </section><section class="section" id="IvoryCoast">
+      <div class="container2">
+        <h2><font color="#FFFFFF">Ivory Coast</font></h2>
+        <div class="section-text">
+        <font color="#FFFFFF">As many other countries around the world, Ivory Coast’s economy was greatly hindered as a result of COVID-19 containment measures. About 40% of businesses were forced to shut down during the first lockdown and many foreign/ export firms closed down causing a large loss of jobs. Nearly 95% of all companies experienced decreased sales throughout the pandemic. Individual households experienced worsened conditions due to difficulties in ability to pay for rent, bills, and food. When visiting Abidjan, Grand-Bassam, and Yamoussoukro. </font>
+        </div>
+        <font color="#FFFFFF"><a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wZnY0dhooagwIBBIIL20vMGZ2NHYSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+         To flights →
+        </a></font>
+      </div>
+    </section><footer class="page-footer">
+     
+     
+    </footer></>);
+}
+
+export default Africa;
