127309eae08553eae8b58f4bc3bbab938c9e3378
gseym <78279867+gseym@users.noreply.github.com>
2022-02-24 23:59:48 -0500

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import App from './App.js';
+import ReactDOM from "react-dom";
+import React from 'react';
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement);
