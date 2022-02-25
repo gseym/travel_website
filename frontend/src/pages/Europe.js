89e2e7a0122a13d3e55ea53e5779232f6f44246c
gseym <78279867+gseym@users.noreply.github.com>
2022-02-25 00:04:55 -0500

    Create Europe.js

---
+++ frontend/src/pages/Europe.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+function Europe() {
+  return (<><div class="container2">
+  <div class="text-intro">
+  <center><h1>Europe</h1></center> 
+    
+  </div>
+  <header class="page-header">
+    <nav>
+      <ul>
+        <li>
+          <a href="#Spain">Spain</a>
+        </li>
+        <li>
+          <a href="#Croatia">Croatia</a>
+        </li>
+        <li>
+          <a href="#Montenegro">Montenegro</a>
+        </li>
+      </ul>
+    </nav>
+    <div class="header-text">
+    Tourism accounts for a substantial portion of the economy of many European countries. Due to this and other sectors being suspended by the pandemic, many countries were economically hindered. If fact, in 2020, the EU real GDP fell by over 6% more than during the global financial crisis. In the countries below, tourism accounts for 10-12 per cent of their GDP.
+    </div>
+  </header>
+</div><section class="section" id="Spain">
+    <div class="container2">
+     
+      <h2><font color="#FFFFFF">Spain</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF"> Despite experiencing a period of economic growth over the last five years pre-Covid, Spain was heavily impacted by the pandemic. Because much of its economy is based on small and medium-sized companies (with less resources and aid), the country is expected to take several years to recovery economically. Popular travel destinations in Spain include Barcelona, Madrid, Seville, and Ibiza. Try to buy from local shops and farmers markets when traveling to the beautiful cities and countryside of Spain! </font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wNm1rahooagwIBBIIL20vMDZta2oSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="Croatia">
+    <div class="container2">
+      <h2><font color="#FFFFFF">Croatia</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF">In addition to national lockdowns caused by the pandemic, Croatia experienced multiple earthquakes in 2020 that brought the country in deeper economic stress. The country saw an especially negative impact due to the extremely large decline of tourism and travel that accounts for a higher share of the country’s GDP than any other EU member. However, in the new year, Croatia is seeing an “economic rebound”.  Popular travel destinations in Croatia include Dubrovnik, Split, and Zagreb. </font>
+      </div>
+      <a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wMXBqNxooagwIBBIIL20vMDFwajcSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+        To flights →
+      </a>
+    </div>
+  </section><section class="section" id="Montenegro">
+    <div class="container2">
+      <h2><font color="#FFFFFF">Montenegro</font></h2>
+      <div class="section-text">
+      <font color="#FFFFFF"> At the height of the pandemic, Montenegro saw some of the highest number of cases in European countries. The country experienced a large economic slowdown that caused a deficit reaching over 7% of the country’s GDP in 2020. This decline was largely due to losses in tourism and employment. Some even described Montenegro as the “Western Balkan country worst affected by the pandemic”. Recommended travel destinations in the country include Motor, Budva, and Podgorica. </font>
+      </div>
+      <font color="#FFFFFF"><a href="https://www.google.com/travel/explore?tfs=CBwQAxooagwIAhIIL20vMDUycDcSCjIwMjItMDItMDdyDAgEEggvbS8wNTZ2dhooagwIBBIIL20vMDU2dnYSCjIwMjItMDItMTFyDAgCEggvbS8wNTJwN3ABggELCP___________wFAAUgBmAEBsgEEGAEgAQ&tfu=GgA&hl=fr" class="section-btn" target="_blank">
+       To flights →
+      </a></font>
+    </div>
+  </section><footer class="page-footer">
+   
+   
+  </footer></>);
+}
+
+export default Europe;
